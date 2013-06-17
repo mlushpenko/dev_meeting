2144337dc3a713a785e72cd8ed427d74b68bb637
saasbook <saasbook@saasbook.(none)>
2013-06-18 00:54:05 +0300

    Simple aws application

---
+++ config/initializers/aws.rb~
@@ -0,0 +1,4 @@
+AWS.config({
+		:access_key_id => 'AKIAJUTSQI5F2VL7QFEQ',
+		:secret_access_key => '87oHDS5hjEXWJgJitGzUEryVAq4myPXhUlk3aEve',
+})
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aws::Application.config.secret_token = '111c1f120c9c6c75709c829fbb3d9ca4bf807cb4ca1effa08aa58c2040e65390b7a0294a92ff5baaf9536d69161123913a550ed8cebb2d0b7ac05a277c89aee2'
